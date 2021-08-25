375f314d3ddb0563bdbd620b2aa262fa236abd04
Luana Andrade <l.andrade2711@gmail.com>
2021-08-24 22:37:53 -0300

    * .gitignore: Aula 1 e 2: Entendendo herança e interface
    * BankByte.sln:
    * Program.cs:
    * BankByte.csproj:
    * GerenciadorBonificacao.cs:

---
+++ BankByte/GerenciadorBonificacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BankByte.Funcionarios;
+
+namespace BankByte
+{
+    public class GerenciadorBonificacao
+    {
+       private double _totalBonificacao;
+
+       public void Registrar(Funcionario funcionario)
+       {
+            _totalBonificacao += funcionario.GetBonificacao();
+       }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
---
+++ BankByte/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BankByte.Funcionarios;
+
+
+namespace BankByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario Carlos = new Funcionario();
+            Carlos.Nome = "Carlos";
+            Carlos.CPF = "546.879.157-20";
+            Carlos.Salario = 2000;
+
+            gerenciador.Registrar(Carlos);
+
+            Diretor Roberta = new Diretor();
+            Roberta.Nome = "Roberta";
+            Roberta.CPF = "454.658.148-3";
+            Roberta.Salario = 5000;
+
+            Funcionario RobertaTeste = Roberta;
+
+            Console.WriteLine("Bonificação de uma referencia de Diretor:  " + Roberta.GetBonificacao());
+            Console.WriteLine("Bonificação de uma referencia de Funcionario:  " + RobertaTeste.GetBonificacao());
+
+
+            gerenciador.Registrar(Roberta);
+
+            //double totalBonificacoes = Carlos.GetBonificacao();
+            //totalBonificacoes += Roberta.GetBonificacao();
+
+            Console.WriteLine(Carlos.Nome);
+            Console.WriteLine(Carlos.GetBonificacao());
+
+            Console.WriteLine(Roberta.Nome);
+            Console.WriteLine(Roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
