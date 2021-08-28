7d5ea9697cb59e64c5e47cf5e88a069c4afdbd61
Luana Andrade <l.andrade2711@gmail.com>
2021-08-28 20:34:54 -0300

    * ParceiroComercial.cs:
    * BankByte.csproj: Aulas finais do curso de Entendendo herança e
      interface

---
+++ BankByte/ParceiroComercial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BankByte.Sistemas;
+
+namespace BankByte
+{
+    public class ParceiroComercial : IAutenticavel
+    {
+      public string Senha { get; set; }
+
+      public bool Autenticar(string senha)
+      {
+            return Senha == senha;
+      }
+    }
+}
